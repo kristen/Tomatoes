5c9bf2114dc2cf063afe6420335a7b28f299d392
Kristen Sundquist <kristen@sigfig.com>
2014-12-13 15:11:58 -0800

    Add url request to rotten tomatoes

--- Tomatoes/FirstViewController.m
+++ Tomatoes/FirstViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FirstViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface FirstViewController ()
 
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURLSessionConfiguration *session = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithSessionConfiguration:session];
+    
+    NSURL *url = [NSURL URLWithString:@"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=95wsptzedh7yfpfx4r537zvu"];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *task = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        NSLog(@"%@", responseObject);
+    }];
+    
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
